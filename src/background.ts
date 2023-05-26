d641b5c606c3701260bc49162de41a764f7d2bb2
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2023-05-26 16:41:55 +0100

    PLATUI-2346: Add permission to disable download shelf

--- src/background.ts
+++ src/background.ts
@@ -4,6 +4,8 @@ chrome.runtime.onMessage.addListener((message) => {
   const url = `data:application/json;base64,${encodedData}`;
   const timestamp = Date.parse(axeResults.timestamp);
 
+  chrome.downloads.setShelfEnabled(false);
+
   chrome.downloads.download({
     url,
     filename: `accessibility-assessment/${timestamp}/axeResults.json`,
