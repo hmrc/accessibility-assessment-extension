99e7b0b924c94e207082b34fd95b2c92c568de0a
Kyle Bowden <1393788+kyle-bowden@users.noreply.github.com>
2024-03-15 15:08:25 +0000

    Add TODO comment to add a test in the future for base64 encoding json

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,12 @@
 chrome.runtime.onMessage.addListener((message) => {
   const axeResults = message.axeResults;
+  /*
+    TODO: add a unit test
+    When encoding a string with btoa (base64), characters outside the Latin1 range, such as äöüÄÖÜçéèñ,
+    lead to an error stating: 'The string to be encoded contains characters outside of the Latin1 range.'
+    To correctly encode UTF-8 strings in base64, it's necessary to first encode the string using encodeURIComponent to escape non-ASCII characters,
+    and then convert the percent-encoded UTF-8 output to a format that btoa can handle
+  */
   const encodedData = btoa(unescape(encodeURIComponent(JSON.stringify(axeResults))));
   const url = `data:application/json;base64,${encodedData}`;
   const timestamp = Date.parse(axeResults.timestamp);
